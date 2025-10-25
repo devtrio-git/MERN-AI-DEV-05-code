792664c292e50f679ba04ddf6d282645ae5af1de
ahmedashar <ahmedashar12345@gmail.com>
2025-10-26 00:55:40 +0500

    arrays and functions

---
+++ Class11-JS/app.js
@@ -0,0 +1,46 @@
+             // 0      //1
+var names = ["Ashar", "Owais", "Jahangir"];
+var ages = [20, 21, 22, 23, 24];
+// console.log(names);
+
+// console.log(names[0])
+// console.log(names[1])
+// console.log(names[2])
+// console.log(names[3])
+
+
+// for (let i = 0; i < names.length ; i++) {
+//     console.log(names[i])
+// }
+
+
+// console.log(names.length) // return length of array
+
+// methods in arrays
+
+// names.push("Mouaz","Zameer");
+// names.push("Ansar");
+// names.pop()
+
+// names.shift()
+// names.unshift("Zameer","Ansar")
+
+// var name = prompt("Enter your name");
+// console.log(name)
+
+// let updatedLength = names.push("Zahoor")
+// console.log(updatedLength)
+
+// let removedValue = names.pop()
+// console.log(removedValue)
+
+
+// let splitArray = ages.slice(0,2)
+// // let splitArray2 = ages.slice(-2)
+// console.log(splitArray);
+// console.log(splitArray2);
+// console.log(ages)
+
+
+// names.splice(1,1,"Asim")
+// console.log(names)
---
+++ Class11-JS/function.js
@@ -0,0 +1,33 @@
+// console.log("Ashar")
+// console.log(26)
+// console.log("Ashar")
+// console.log(26)
+// console.log("Ashar")
+// console.log(26)
+// console.log("Ashar")
+// console.log(26)
+// console.log("Ashar")
+// console.log(26)
+
+// function printMyNameAndAge() {
+//     console.log("Ashar")
+//     console.log(26)
+// }
+
+// function add(num1,num2){ // parameters
+//     num1+num2
+//     return num1+num2
+// }
+
+// console.log(add(4,5)) // argument
+// add(100,30) // argument
+
+
+// function ageVerificationForVote(){
+//    var age = prompt("How old are you?");
+//    if(age >= 18){
+//     alert("You are eligible for vote ✅")
+//    }else{
+//     alert("Sorry! you are not eligible for vote ❌")
+//    }
+// }
