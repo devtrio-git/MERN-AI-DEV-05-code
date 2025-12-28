76a2b0875e388d738d3985460d9c39e43ab8e673
ahmedashar <ahmedashar12345@gmail.com>
2025-12-29 01:54:17 +0500

    Add Class 25 react js 02

---
+++ Class25-ReactJS/fundamentalReact/src/App.jsx
@@ -0,0 +1,25 @@
+import PrimaryButton from "./components/buttons/PrimaryButton"
+import Counter from "./components/counter/Counter"
+import Navbar from "./components/navbar/Navbar"
+
+function App() {
+
+  return (
+    <>
+      {/* <Navbar />
+    <div className="container">
+      <h1>Ashar Ahmed</h1>
+      <div className="d-flex">
+      <PrimaryButton title="Login" text="asdasd"/>
+
+      <PrimaryButton title="Signup" ></PrimaryButton>
+      </div>
+    </div> */}
+
+    <Counter />
+
+    </>
+  )
+}
+
+export default App
